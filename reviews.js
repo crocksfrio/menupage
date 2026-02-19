6880e902650896e2695cf2dcd17bd750a0d759a0
crocksfrio <crocksfrio.oficial@gmail.com>
2026-02-19 04:57:30 -0300

    Social Sync: 5 avaliações totais (19/02/2026 04:57:29)

--- reviews.js
+++ reviews.js
@@ -1,24 +1,43 @@
-// Atualizado em: Thu Feb 19 04:49:07 2026
+// Gerado automaticamente - 19/02/2026 04:57:29
 const GOOGLE_REVIEWS_DATA = [
     {
-        "nome": "julio cesar",
-        "texto": "Top 👍🏽❤️",
+        "nome": "Heloiza Cabral",
+        "texto": "Excelente atendimento e qualidade!",
         "estrelas": 5,
-        "foto_perfil": "https://lh3.googleusercontent.com/a-/ALV-UjVPw58Yx7cgwktWgQ5NF4hNtoc4c9rEwQjInfQiFxTqf5em6vnx=s128-c0x00000000-cc-rp-mo-ba2",
-        "data_relativa": "3 months ago"
+        "foto_perfil": "https://lh3.googleusercontent.com/a-/ALV-UjXeQY5atJT7H_G_W4sXbk5sZ38LfTvcN-dAX5F-_I_7rryBDR2v=s128-c0x00000000-cc-rp-mo",
+        "data_relativa": "in the last week",
+        "uid": "b47ba4b2b539654dd9dbb05ada5f243a"
     },
     {
-        "nome": "Alice Maia",
-        "texto": "Atendimento nota 10, sorvetes deliciosos e ambiente super aconchegante.\nCom certeza volto la em breve",
+        "nome": "Daniele Andrade Felix",
+        "texto": "Ótimo para levar as crianças 👍🏻",
         "estrelas": 5,
-        "foto_perfil": "https://lh3.googleusercontent.com/a-/ALV-UjV6smhEZNB8VRzdrCMmvZRRdE_s_7GYn5Fm98TCMEgcXtSu9bZBkg=s128-c0x00000000-cc-rp-mo-ba4",
-        "data_relativa": "a week ago"
+        "foto_perfil": "https://lh3.googleusercontent.com/a-/ALV-UjXEb1GAyXx-ZZMSdBG1Fu-HLq3MZygofHGbfsZu0pSP_XNySTGHmw=s128-c0x00000000-cc-rp-mo",
+        "data_relativa": "in the last week",
+        "uid": "95f4d1aedba8459e2c28ff0bb487948b"
     },
     {
-        "nome": "Silas Beserra",
-        "texto": "Lugar mega agradável! Sabor incrível!! Destaque para a massa maravilhosa do croissants e as opções de sorvete sem lactose.",
+        "nome": "Helio Valentini",
+        "texto": "Excelente sorveteria👏🏻 Localização, ambiente, atendimento e sorvete de qualidade",
         "estrelas": 5,
-        "foto_perfil": "https://lh3.googleusercontent.com/a-/ALV-UjW8a8DvvUARLeX3UTKalubLYI_eelupB9BY2ATYlhJ-Nr5_F2Zaeg=s128-c0x00000000-cc-rp-mo-ba2",
-        "data_relativa": "4 months ago"
+        "foto_perfil": "https://lh3.googleusercontent.com/a-/ALV-UjXkvCp_Ms873Ni6tUfzixzy5pit8wRORxKvMK6QEo4PqOvWlBM=s128-c0x00000000-cc-rp-mo",
+        "data_relativa": "in the last week",
+        "uid": "704798094343905191ec1476841b7cfd"
+    },
+    {
+        "nome": "Lívia Fragoso",
+        "texto": "Excelente atendimento, ambiente e sabor!",
+        "estrelas": 5,
+        "foto_perfil": "https://lh3.googleusercontent.com/a-/ALV-UjXLjosCc7o25uU4wnemLmCqycODP0teHrLfbvroHGwlMWSwfFCQ=s128-c0x00000000-cc-rp-mo",
+        "data_relativa": "in the last week",
+        "uid": "f01140ec1b78b8cb0ef5e62e2cd01497"
+    },
+    {
+        "nome": "Antonella Andreazza",
+        "texto": "otimo atendimento e nos deram dicas otimas de Recife! varias frutas locais",
+        "estrelas": 5,
+        "foto_perfil": "https://lh3.googleusercontent.com/a-/ALV-UjUE3Cq8oznypTOgCnwtT-yani1G--X_oFJNIrgjh27DU10DDNY=s128-c0x00000000-cc-rp-mo",
+        "data_relativa": "in the last week",
+        "uid": "e77ee4ccfc57dda26fab5e4d876145fd"
     }
 ];
