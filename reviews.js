9e43df132ae53e38d44bfb5b5c2a94f98b1ff850
crocksfrio <crocksfrio.oficial@gmail.com>
2026-02-19 04:49:08 -0300

    Update: Sincronização automática de avaliações reais

---
+++ reviews.js
@@ -0,0 +1,24 @@
+// Atualizado em: Thu Feb 19 04:49:07 2026
+const GOOGLE_REVIEWS_DATA = [
+    {
+        "nome": "julio cesar",
+        "texto": "Top 👍🏽❤️",
+        "estrelas": 5,
+        "foto_perfil": "https://lh3.googleusercontent.com/a-/ALV-UjVPw58Yx7cgwktWgQ5NF4hNtoc4c9rEwQjInfQiFxTqf5em6vnx=s128-c0x00000000-cc-rp-mo-ba2",
+        "data_relativa": "3 months ago"
+    },
+    {
+        "nome": "Alice Maia",
+        "texto": "Atendimento nota 10, sorvetes deliciosos e ambiente super aconchegante.\nCom certeza volto la em breve",
+        "estrelas": 5,
+        "foto_perfil": "https://lh3.googleusercontent.com/a-/ALV-UjV6smhEZNB8VRzdrCMmvZRRdE_s_7GYn5Fm98TCMEgcXtSu9bZBkg=s128-c0x00000000-cc-rp-mo-ba4",
+        "data_relativa": "a week ago"
+    },
+    {
+        "nome": "Silas Beserra",
+        "texto": "Lugar mega agradável! Sabor incrível!! Destaque para a massa maravilhosa do croissants e as opções de sorvete sem lactose.",
+        "estrelas": 5,
+        "foto_perfil": "https://lh3.googleusercontent.com/a-/ALV-UjW8a8DvvUARLeX3UTKalubLYI_eelupB9BY2ATYlhJ-Nr5_F2Zaeg=s128-c0x00000000-cc-rp-mo-ba2",
+        "data_relativa": "4 months ago"
+    }
+];
